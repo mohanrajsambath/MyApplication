373c36f6ae0515ad1e55cd4ccb78689ac16b697c
sathish <sathish.engineer1@gmail.com>
2019-10-09 22:38:32 +0200

    Changes done in BlogViewModel

--- livedata/src/main/java/com/sathish/livedata/viewmodel/BlogViewModel.kt
+++ livedata/src/main/java/com/sathish/livedata/viewmodel/BlogViewModel.kt
@@ -1,8 +1,11 @@
 package com.sathish.livedata.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sathish.livedata.service.model.BlogWrapper
 
 class BlogViewModel : ViewModel() {
+    lateinit var allblog:MutableLiveData<BlogWrapper>
 
 
 
